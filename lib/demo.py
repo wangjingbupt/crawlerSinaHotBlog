0609f7d5b0aa4d624c829086d8e9505f661dbc83
Wang Jing <wangjing@i-2-22266-VM.cs2cloud.internal>
2013-06-18 18:02:45 +0800

    update

--- lib/demo.py
+++ lib/demo.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 # encoding=utf8 
 import sys,time
+sys.path.append("./crawler")
 reload(sys)
 sys.setdefaultencoding('utf-8')
-sys.path.append("./crawler")
 from crawlerSinaBlog import CrawlerSinaBlog
+from  datetime  import  * 
 
-import pymongo
+import pymongo,time
+
+host = '127.0.0.1'
+port = 27017
+conn = pymongo.Connection(host, port)
+print '------------------'
+print datetime.today()
 
 a = CrawlerSinaBlog()
 seedUrl = a.getSeedUrl()
@@ -16,7 +23,24 @@ if len(seedUrl) > 0:
     a.getPostUrl(url)
 print len(a.urls)
 if len(a.urls) > 0 :
+  count = 1
   for url in a.urls:
-    body = a.getPostBody(url)
-    print body
-    sys.exit()
+    pos = url.find('http://blog.sina.com.cn/s/blog_')
+    if pos < 0:
+      continue
+    pos +=31
+    pos_end = pos + url[pos:].find('.') 
+    blogId = url[pos:pos_end]
+    s = conn.blog.post.find_one({'blogId':blogId})
+    if not s:
+      body = a.getPostBody(url)
+      doc = {'blogId':blogId,'content':body}
+      conn.blog.post.insert(doc)
+      count += 1
+    if count % 20 == 0:
+      time.sleep(5)
+
+    #print body
+print (count-1)
+print datetime.today()
+print ''
